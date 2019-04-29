6fd9e540dc3cd2edb7e540d660e7372f0ae9992d
Memo Cachú <gcachu.o@gmail.com>
2019-04-29 14:05:41 -0500

    get page

--- assets/src/ts/core/project.ts
+++ assets/src/ts/core/project.ts
@@ -4,6 +4,8 @@ $(function () {
 
 export class Project {
     static init() {
-
+        console.log(location);
+        const pathname = location.pathname;
+        const search = location.search;
     }
 }
